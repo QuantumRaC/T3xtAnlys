d764d6cbf7c0ce563674f07e7f7d04785d2a69f9
QuantumRac <cynthiayao11@outlook.com>
2025-09-25 22:42:00 -0400

    Fixed SlowAPI request parameter

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # during dev allow all; in prod restrict to your site
+    allow_origins=["https://raccfields.vercel.app/"],   # during dev allow all; in prod restrict to site
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -45,24 +45,26 @@ async def root():
 
 @app.post("/analyze")
 @limiter.limit("30/minute;500/day")
-async def analyze_text(payload: dict = Body(...)):
+async def analyze_text(payload: dict = Body(...), request: Request):
     text = payload.get("text", "")
     if not text.strip():
         return {"error": "No text provided."}
-        # in case of empty input, return error instead of sending it to GenAI
-    
+
     lang = detect(text)
     print(text)
     if lang == "en":
-        if(not nlp_en):
+        global nlp_en
+        if not nlp_en:
             nlp_en = spacy.load("en_core_web_md")
         prompt = text_analyze_eng(text, nlp_en)
     elif lang == "zh-cn":
-        if(not nlp_cn):
+        global nlp_cn
+        if not nlp_cn:
             nlp_cn = spacy.load("zh_core_web_sm")
         prompt = text_analyze_chn(text, nlp_cn)
     else:
         return {"error": f"Unsupported language detected: {lang}"}
+    
     try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
@@ -70,4 +72,5 @@ async def analyze_text(payload: dict = Body(...)):
         )
     except Exception as e:
         return {"error": f"AI generation failed: {str(e)}"}
+    
     return {"language": lang, "analysis": response.text}
