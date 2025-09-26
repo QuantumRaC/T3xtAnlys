cf944ab05dcfd007fd09def8c403f4152f10e838
QuantumRac <cynthiayao11@outlook.com>
2025-09-25 22:53:57 -0400

    Testing with allow_origins in CORSMiddleware

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://raccfields.vercel.app/"],   # during dev allow all; in prod restrict to site
+    allow_origins=["*"],   # during dev allow all; in prod restrict to site
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
